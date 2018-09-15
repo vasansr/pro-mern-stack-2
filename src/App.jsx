5822875f2e81bfb8b65d0fdb9542c88ac05bda6b
Vasan Subramanian <vasan.srini@gmail.com>
2018-09-15 17:43:56 +0530

    03.2 - Composing Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,38 @@
+class IssueFilter extends React.Component {
+  render() {
+    return (
+      <div>This is a placeholder for the issue filter.</div>
+    );
+  }
+}
+
+class IssueTable extends React.Component {
+  render() {
+    return (
+      <div>This is a placeholder for a table of issues.</div>
+    );
+  }
+}
+
+class IssueAdd extends React.Component {
+  render() {
+    return (
+      <div>This is a placeholder for a form to add an issue.</div>
+    );
+  }
+}
+
 class IssueList extends React.Component {
   render() {
     return (
-      <div>This is a placeholder for the issue list.</div>
+      <React.Fragment>
+        <h1>Issue Tracker</h1>
+        <IssueFilter />
+        <hr />
+        <IssueTable />
+        <hr />
+        <IssueAdd />
+      </React.Fragment>
     );
   }
 }
