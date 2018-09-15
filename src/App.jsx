b991051ca34f7ba9449689ff1f02528059830f69
Vasan Subramanian <vasan.srini@gmail.com>
2018-09-15 20:11:14 +0530

    03.3 - Passing Data Using Properties

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,36 @@ class IssueFilter extends React.Component {
   }
 }
 
+class IssueRow extends React.Component {
+  render() {
+    const style = this.props.rowStyle;
+    return (
+      <tr>
+        <td style={style}>{this.props.issue_id}</td>
+        <td style={style}>{this.props.issue_title}</td>
+      </tr>
+    );
+  }
+}
+
 class IssueTable extends React.Component {
   render() {
+    const rowStyle = {border: "1px solid silver", padding: 4};
     return (
-      <div>This is a placeholder for a table of issues.</div>
+      <table style={{borderCollapse: "collapse"}}>
+        <thead>
+          <tr>
+            <th style={rowStyle}>ID</th>
+            <th style={rowStyle}>Title</th>
+          </tr>
+        </thead>
+        <tbody>
+          <IssueRow rowStyle={rowStyle} issue_id={1}
+            issue_title="Error in console when clicking Add" />
+          <IssueRow rowStyle={rowStyle} issue_id={2}
+            issue_title="Missing bottom border on panel" />
+        </tbody>
+      </table>
     );
   }
 }
