ae34f1d83b4a438b49a55388fb8dc642289385b1
Vasan Subramanian <vasan.srini@gmail.com>
2018-09-30 18:09:09 +0530

    05.4 - Integrate List API

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,3 @@
-const initialIssues = [
-  {
-    id: 1, status: 'Open', owner: 'Ravan', effort: 5,
-    created: new Date('2018-08-15'), due: undefined,
-    title: 'Error in console when clicking Add',
-  },
-  {
-    id: 2, status: 'Assigned', owner: 'Eddie', effort: 14,
-    created: new Date('2018-08-16'), due: new Date('2018-08-30'),
-    title: 'Missing bottom border on panel',
-  },
-];
-
 class IssueFilter extends React.Component {
   render() {
     return (
@@ -26,9 +13,9 @@ function IssueRow(props) {
       <td>{issue.id}</td>
       <td>{issue.status}</td>
       <td>{issue.owner}</td>
-      <td>{issue.created.toDateString()}</td>
+      <td>{issue.created}</td>
       <td>{issue.effort}</td>
-      <td>{issue.due ? issue.due.toDateString() : ''}</td>
+      <td>{issue.due}</td>
       <td>{issue.title}</td>
     </tr>
   );
@@ -97,10 +84,24 @@ class IssueList extends React.Component {
     this.loadData();
   }
 
-  loadData() {
-    setTimeout(() => {
-      this.setState({ issues: initialIssues });
-    }, 500);
+  async loadData() {
+    const query = `
+      query {
+        issueList {
+          id status owner
+          created effort due
+          title
+        }
+      }
+    `
+
+    const response = await fetch('/graphql', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json'},
+      body: JSON.stringify({ query })
+    });
+    const result = await response.json();
+    this.setState({ issues: result.data.issueList });
   }
 
   createIssue(issue) {
