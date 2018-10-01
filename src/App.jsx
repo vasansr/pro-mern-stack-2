de27e0e6f9efd4b19c94312fb18d86d8d0c373d4
Vasan Subramanian <vasan.srini@gmail.com>
2018-10-01 15:21:03 +0530

    05.8 - Query Variables

--- src/App.jsx
+++ src/App.jsx
@@ -111,20 +111,18 @@ class IssueList extends React.Component {
   }
 
   async createIssue(issue) {
-    const query = `mutation {
-      issueAdd(issue:{
-        title: "${issue.title}",
-        owner: "${issue.owner}",
-        due: "${issue.due.toISOString()}",
-      }) {
+    const query = `mutation issueAdd($issue: IssueInputs!) {
+      issueAdd(issue: $issue) {
         id
       }
     }`;
 
+    const variables = { issue };
+
     const response = await fetch('/graphql', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json'},
-      body: JSON.stringify({ query })
+      body: JSON.stringify({ query, variables })
     });
     this.loadData();
   }
