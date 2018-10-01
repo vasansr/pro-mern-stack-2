bd5f5a470b6ef67d3138aef64eedd81613cca9a9
Vasan Subramanian <vasan.srini@gmail.com>
2018-10-01 14:40:41 +0530

    05.7 - Integrate Create API

--- src/App.jsx
+++ src/App.jsx
@@ -64,6 +64,7 @@ class IssueAdd extends React.Component {
     const form = document.forms.issueAdd;
     const issue = {
       owner: form.owner.value, title: form.title.value, status: 'New',
+      due: new Date(new Date().getTime() + 1000*60*60*24*10),
     }
     this.props.createIssue(issue);
     form.owner.value = ""; form.title.value = "";
@@ -109,12 +110,23 @@ class IssueList extends React.Component {
     this.setState({ issues: result.data.issueList });
   }
 
-  createIssue(issue) {
-    issue.id = this.state.issues.length + 1;
-    issue.created = new Date();
-    const newIssueList = this.state.issues.slice();
-    newIssueList.push(issue);
-    this.setState({ issues: newIssueList });
+  async createIssue(issue) {
+    const query = `mutation {
+      issueAdd(issue:{
+        title: "${issue.title}",
+        owner: "${issue.owner}",
+        due: "${issue.due.toISOString()}",
+      }) {
+        id
+      }
+    }`;
+
+    const response = await fetch('/graphql', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json'},
+      body: JSON.stringify({ query })
+    });
+    this.loadData();
   }
 
   render() {
