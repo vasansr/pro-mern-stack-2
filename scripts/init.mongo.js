b3d83ad890c48c1512f45d2ffa9a48e25d6432a4
Vasan Subramanian <vasan.srini@gmail.com>
2018-10-08 19:24:56 +0530

    06.06 - Writing to MongoDB

--- scripts/init.mongo.js
+++ scripts/init.mongo.js
@@ -19,6 +19,9 @@ db.issues.insertMany(issuesDB);
 const count = db.issues.count();
 print('Inserted', count, 'issues');
 
+db.counters.remove({ _id: 'issues' });
+db.counters.insert({ _id: 'issues', current: count });
+
 db.issues.createIndex({ id: 1 }, { unique: true });
 db.issues.createIndex({ status: 1 });
 db.issues.createIndex({ owner: 1 });
--- server/server.js
+++ server/server.js
@@ -11,19 +11,6 @@ let db;
 
 let aboutMessage = "Issue Tracker API v1.0";
 
-const issuesDB = [
-  {
-    id: 1, status: 'Open', owner: 'Ravan', effort: 5,
-    created: new Date('2019-01-15'), due: undefined,
-    title: 'Error in console when clicking Add',
-  },
-  {
-    id: 2, status: 'Assigned', owner: 'Eddie', effort: 14,
-    created: new Date('2019-01-16'), due: new Date('2019-02-01'),
-    title: 'Missing bottom border on panel',
-  },
-];
-
 const GraphQLDate = new GraphQLScalarType({
   name: 'GraphQLDate',
   description: 'A Date() type in GraphQL as a scalar',
@@ -63,7 +50,16 @@ async function issueList() {
   return issues;
 }
 
-function issueAdd(_, { issue }) {
+async function getNextSequence(name) {
+  const result = await db.collection('counters').findOneAndUpdate(
+    { _id: name },
+    { $inc: { current: 1 } },
+    { returnOriginal: false },
+  );
+  return result.value.current;
+}
+
+async function issueAdd(_, { issue }) {
   const errors = [];
   if (issue.title.length < 3) {
     errors.push('Field "title" must be at least 3 characters long.')
@@ -75,9 +71,13 @@ function issueAdd(_, { issue }) {
     throw new UserInputError('Invalid input(s)', { errors });
   }
   issue.created = new Date();
-  issue.id = issuesDB.length + 1;
-  issuesDB.push(issue);
-  return issue;
+  issue.id = await getNextSequence('issues');
+
+  const result = await db.collection('issues').insertOne(issue);
+  const savedIssue = await db.collection('issues').findOne({
+    _id: result.insertedId
+  });
+  return savedIssue;
 }
 
 async function connectToDb() {
