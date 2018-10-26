04e6f6e7e66416bc5da09618d560eacee7f74a0e
Vasan Subramanian <vasan.srini@gmail.com>
2018-10-26 22:35:35 +0530

    07.08 - Hot Module Replacement

--- ui/uiserver.js
+++ ui/uiserver.js
@@ -4,6 +4,24 @@ const proxy = require('http-proxy-middleware');
 
 const app = express();
 
+const enableHMR = (process.env.ENABLE_HMR || 'true') === 'true';
+
+if (enableHMR && (process.env.NODE_ENV !== 'production')) {
+  console.log('Adding dev middlware, enabling HMR');
+  const webpack = require('webpack');
+  const devMiddleware = require('webpack-dev-middleware');
+  const hotMiddleware = require('webpack-hot-middleware');
+
+  const config = require('./webpack.config');
+  config.entry.app.push('webpack-hot-middleware/client');
+  config.plugins = config.plugins || [];
+  config.plugins.push(new webpack.HotModuleReplacementPlugin())
+
+  const compiler = webpack(config);
+  app.use(devMiddleware(compiler));
+  app.use(hotMiddleware(compiler));
+}
+
 app.use(express.static('public'));
 
 const apiProxyTarget = process.env.API_PROXY_TARGET;
