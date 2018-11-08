ec74a4303f09f0fcfbf72f4e4bc938908a33f373
Vasan Subramanian <vasan.srini@gmail.com>
2018-11-08 17:35:08 +0530

    09.05 - Programmatic Navigation

--- ui/src/IssueFilter.jsx
+++ ui/src/IssueFilter.jsx
@@ -1,22 +1,36 @@
-/* eslint "react/prefer-stateless-function": "off" */
-
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 
-export default class IssueFilter extends React.Component {
+class IssueFilter extends React.Component {
+  constructor() {
+    super();
+    this.onChangeStatus = this.onChangeStatus.bind(this);
+  }
+
+  onChangeStatus(e) {
+    const status = e.target.value;
+    const { history } = this.props;
+    history.push({
+      pathname: '/issues',
+      search: status ? `?status=${status}` : '',
+    });
+  }
+
   render() {
     return (
       <div>
-        <Link to="/issues">All Issues</Link>
-        {' | '}
-        <Link to={{ pathname: '/issues', search: '?status=New' }}>
-          New Issues
-        </Link>
-        {' | '}
-        <Link to={{ pathname: '/issues', search: '?status=Assigned' }}>
-          Assigned Issues
-        </Link>
+        Show status:
+        {' '}
+        <select onChange={this.onChangeStatus}>
+          <option value="">(All)</option>
+          <option value="New">New</option>
+          <option value="Assigned">Assigned</option>
+          <option value="Fixed">Fixed</option>
+          <option value="Closed">Closed</option>
+        </select>
       </div>
     );
   }
 }
+
+export default withRouter(IssueFilter);
