6758e7b23ffa313ab0b399182d811569118f7751
Vasan Subramanian <vasan.srini@gmail.com>
2018-11-10 11:29:50 +0530

    09.07 - Browser History Router

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -2,7 +2,7 @@ import 'babel-polyfill';
 import 'whatwg-fetch';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import Page from './Page.jsx';
 
--- ui/uiserver.js
+++ ui/uiserver.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const path = require('path');
 const express = require('express');
 const proxy = require('http-proxy-middleware');
 
@@ -37,6 +38,10 @@ const env = { UI_API_ENDPOINT };
 
 app.get('/env.js', (req, res) => {
   res.send(`window.ENV = ${JSON.stringify(env)}`);
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve('public/index.html'));
 });
 
 const port = process.env.UI_SERVER_PORT || 8000;
