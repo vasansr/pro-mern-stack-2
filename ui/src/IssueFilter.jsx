45c657538558346900173c1c025efef3372eab19
Vasan Subramanian <vasan.srini@gmail.com>
2018-11-21 15:54:48 +0530

    10.01 - Controlled Components

--- ui/src/IssueFilter.jsx
+++ ui/src/IssueFilter.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import URLSearchParams from 'url-search-params';
 import { withRouter } from 'react-router-dom';
 
 class IssueFilter extends React.Component {
@@ -17,11 +18,13 @@ class IssueFilter extends React.Component {
   }
 
   render() {
+    const { location: { search } } = this.props;
+    const params = new URLSearchParams(search);
     return (
       <div>
         Status:
         {' '}
-        <select onChange={this.onChangeStatus}>
+        <select value={params.get('status') || ''} onChange={this.onChangeStatus}>
           <option value="">(All)</option>
           <option value="New">New</option>
           <option value="Assigned">Assigned</option>
