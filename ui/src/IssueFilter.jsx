6fa525a9406c8af2581280bd9b7613d1af48c20c
Vasan Subramanian <vasan.srini@gmail.com>
2018-11-22 17:41:51 +0530

    10.04 - Typed Inputs

--- ui/src/IssueFilter.jsx
+++ ui/src/IssueFilter.jsx
@@ -8,10 +8,14 @@ class IssueFilter extends React.Component {
     const params = new URLSearchParams(search);
     this.state = {
       status: params.get('status') || '',
+      effortMin: params.get('effortMin') || '',
+      effortMax: params.get('effortMax') || '',
       changed: false,
     };
 
     this.onChangeStatus = this.onChangeStatus.bind(this);
+    this.onChangeEffortMin = this.onChangeEffortMin.bind(this);
+    this.onChangeEffortMax = this.onChangeEffortMax.bind(this);
     this.applyFilter = this.applyFilter.bind(this);
     this.showOriginalFilter = this.showOriginalFilter.bind(this);
   }
@@ -28,26 +32,46 @@ class IssueFilter extends React.Component {
     this.setState({ status: e.target.value, changed: true });
   }
 
+  onChangeEffortMin(e) {
+    const effortString = e.target.value;
+    if (effortString.match(/^\d*$/)) {
+      this.setState({ effortMin: e.target.value, changed: true });
+    }
+  }
+
+  onChangeEffortMax(e) {
+    const effortString = e.target.value;
+    if (effortString.match(/^\d*$/)) {
+      this.setState({ effortMax: e.target.value, changed: true });
+    }
+  }
+
   showOriginalFilter() {
     const { location: { search } } = this.props;
     const params = new URLSearchParams(search);
     this.setState({
       status: params.get('status') || '',
+      effortMin: params.get('effortMin') || '',
+      effortMax: params.get('effortMax') || '',
       changed: false,
     });
   }
 
   applyFilter() {
-    const { status } = this.state;
+    const { status, effortMin, effortMax } = this.state;
     const { history } = this.props;
-    history.push({
-      pathname: '/issues',
-      search: status ? `?status=${status}` : '',
-    });
+    const params = new URLSearchParams();
+    if (status) params.set('status', status);
+    if (effortMin) params.set('effortMin', effortMin);
+    if (effortMax) params.set('effortMax', effortMax);
+
+    const search = params.toString() ? `?${params.toString()}` : '';
+    history.push({ pathname: '/issues', search });
   }
 
   render() {
     const { status, changed } = this.state;
+    const { effortMin, effortMax } = this.state;
     return (
       <div>
         Status:
@@ -59,6 +83,20 @@ class IssueFilter extends React.Component {
           <option value="Fixed">Fixed</option>
           <option value="Closed">Closed</option>
         </select>
+        {' '}
+        Effort between:
+        {' '}
+        <input
+          size={5}
+          value={effortMin}
+          onChange={this.onChangeEffortMin}
+        />
+        {' - '}
+        <input
+          size={5}
+          value={effortMax}
+          onChange={this.onChangeEffortMax}
+        />
         {' '}
         <button type="button" onClick={this.applyFilter}>Apply</button>
         {' '}
--- ui/src/IssueList.jsx
+++ ui/src/IssueList.jsx
@@ -33,8 +33,21 @@ export default class IssueList extends React.Component {
     const vars = {};
     if (params.get('status')) vars.status = params.get('status');
 
-    const query = `query issueList($status: StatusType) {
-      issueList (status: $status) {
+    const effortMin = parseInt(params.get('effortMin'), 10);
+    if (!Number.isNaN(effortMin)) vars.effortMin = effortMin;
+    const effortMax = parseInt(params.get('effortMax'), 10);
+    if (!Number.isNaN(effortMax)) vars.effortMax = effortMax;
+
+    const query = `query issueList(
+      $status: StatusType
+      $effortMin: Int
+      $effortMax: Int
+    ) {
+      issueList(
+        status: $status
+        effortMin: $effortMin
+        effortMax: $effortMax
+      ) {
         id title status owner
         created effort due
       }
