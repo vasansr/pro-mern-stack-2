a756c297bf5b87a98920a2afaa9a7f8c625042a2
Vasan Subramanian <vasan.srini@gmail.com>
2018-12-16 22:52:36 +0530

    12.05 - Server Router

--- ui/server/render.jsx
+++ ui/server/render.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
+import { StaticRouter } from 'react-router-dom';
 
-import About from '../src/About.jsx';
+import Page from '../src/Page.jsx';
 import template from './template.js';
 
 function render(req, res) {
-  const body = ReactDOMServer.renderToString(<About />);
+  const element = (
+    <StaticRouter location={req.url} context={{}}>
+      <Page />
+    </StaticRouter>
+  );
+  const body = ReactDOMServer.renderToString(element);
   res.send(template(body));
 }
 
