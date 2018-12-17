cec05de206784c904084bd1f1c61d20e21a05e5e
Vasan Subramanian <vasan.srini@gmail.com>
2018-12-17 11:09:35 +0530

    12.09 - Common Data Fetcher

--- ui/server/render.jsx
+++ ui/server/render.jsx
@@ -4,11 +4,11 @@ import { StaticRouter } from 'react-router-dom';
 
 import Page from '../src/Page.jsx';
 import template from './template.js';
-import graphQLFetch from '../src/graphQLFetch.js';
+import About from '../src/About.jsx';
 import store from '../src/store.js';
 
 async function render(req, res) {
-  const initialData = await graphQLFetch('query{about}');
+  const initialData = About.fetchData();
   store.initialData = initialData;
   const element = (
     <StaticRouter location={req.url} context={{}}>
--- ui/src/About.jsx
+++ ui/src/About.jsx
@@ -1,13 +1,36 @@
 import React from 'react';
 import store from './store.js';
+import graphQLFetch from './graphQLFetch.js';
 
-export default function About() {
-  return (
-    <div className="text-center">
-      <h3>Issue Tracker version 0.9</h3>
-      <h4>
-        {store.initialData ? store.initialData.about : 'unknown'}
-      </h4>
-    </div>
-  );
+export default class About extends React.Component {
+  static async fetchData() {
+    const data = await graphQLFetch('query {about}');
+    return data;
+  }
+
+  constructor(props) {
+    super(props);
+    const apiAbout = store.initialData ? store.initialData.about : null;
+    this.state = { apiAbout };
+  }
+
+  async componentDidMount() {
+    const { apiAbout } = this.state;
+    if (apiAbout == null) {
+      const data = await About.fetchData();
+      this.setState({ apiAbout: data.about });
+    }
+  }
+
+  render() {
+    const { apiAbout } = this.state;
+    return (
+      <div className="text-center">
+        <h3>Issue Tracker version 0.9</h3>
+        <h4>
+          {apiAbout}
+        </h4>
+      </div>
+    );
+  }
 }
