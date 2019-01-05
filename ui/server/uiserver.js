33ed36a5543beb47680a81e074aa581f3ee6135b
Vasan Subramanian <vasan.srini@gmail.com>
2019-01-05 16:41:11 +0530

    15.04 - Proxy Mode

--- ui/server/uiserver.js
+++ ui/server/uiserver.js
@@ -33,8 +33,8 @@ app.use(express.static('public'));
 
 const apiProxyTarget = process.env.API_PROXY_TARGET;
 if (apiProxyTarget) {
-  app.use('/graphql', proxy({ target: apiProxyTarget }));
-  app.use('/auth', proxy({ target: apiProxyTarget }));
+  app.use('/graphql', proxy({ target: apiProxyTarget, changeOrigin: true }));
+  app.use('/auth', proxy({ target: apiProxyTarget, changeOrigin: true }));
 }
 
 if (!process.env.UI_API_ENDPOINT) {
